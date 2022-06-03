f75d4db10aee07fc42820a83d6661f632d2d9b25
Link <clcreta@ctemplar.com>
2022-06-03 15:19:04 +0800

    Some Changes

--- som.py
+++ som.py
@@ -19,6 +19,7 @@ class SomInventory:
         if selected == "All" and index_title == None:
             index_title = "All"
             data = data
+            data = data.sort_values(by=['1'], ascending=False)
             data.index+=1
         
         if selected == "Skills Lab Inventory" and index_title == None:
