e095a7997ba1f4668a40c077327b9c5bbf5ce14a
Link <clcreta@ctemplar.com>
2022-06-03 14:57:10 +0800

    Some Changes

--- som.py
+++ som.py
@@ -10,7 +10,6 @@ class SomInventory:
         index_title = None
         rows = InventoryData.load_data()
         df = pd.DataFrame(rows)
-        data = df[['Item','Quantity','Commonly_Used_By','Location']]
 
         with st.sidebar:
             selected = option_menu("Main Menu", ["All","Skills Lab Inventory", "Dean's Office Inventory", 'Stock Room Inventory', 'Models Inventory', 'Microscope Inventory'], 
@@ -18,6 +17,12 @@ class SomInventory:
         
         if selected == "All" and index_title == None:
             index_title = "All"
+            data = df[['Item','Quantity','Commonly_Used_By','Location']]
+        
+        if selected == "Skills Lab Inventory" and index_title == None:
+            index_title = "Skills Lab Inventory"
+            data = df[df['location'] == "Skills Lab"]
+
 
         #data2 = data[data['Location'] == "Stock Room"]
         #data.index+=1
