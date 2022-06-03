f504e2a34c5e84b16d4082b794ba6cdd5e303437
Link <clcreta@ctemplar.com>
2022-06-03 14:36:32 +0800

    Some Changes

--- som.py
+++ som.py
@@ -8,8 +8,8 @@ st.set_page_config(
         page_title="SOM-Inventory",
 )
 with st.sidebar:
-    selected = option_menu("Main Menu", ["Skills Lab Inventory", "Dean's Office Inventory", 'Stock Room Inventory', 'Models Inventory', 'Microscope Inventory'], 
-        icons=['journal-medical', 'journal-medical', 'journal-medical', 'journal-medical', 'journal-medical'], menu_icon="calendar4-week", default_index=1)
+    selected = option_menu("Main Menu", ["All","Skills Lab Inventory", "Dean's Office Inventory", 'Stock Room Inventory', 'Models Inventory', 'Microscope Inventory'], 
+        icons=['journal-medical', 'journal-medical', 'journal-medical', 'journal-medical', 'journal-medical'], menu_icon="calendar4-week", default_index=0)
 st.title("Inventory") 
 rows = InventoryData.load_data()
 df = pd.DataFrame(rows)
