7955f594e582a30484a8e8efb633f290ddf7323e
Link <clcreta@ctemplar.com>
2022-06-03 14:48:09 +0800

    Some Changes

--- som.py
+++ som.py
@@ -5,22 +5,27 @@ from inventorydata import InventoryData
 from streamlit_option_menu import option_menu
 
 class SomInventory:
-    
+
     def inventory_list():
+        index_title = None
+        rows = InventoryData.load_data()
+        df = pd.DataFrame(rows)
+        data = df[['Item','Quantity','Commonly_Used_By','Location']]
 
         st.set_page_config(
                 page_title="SOM-Inventory",
         )
+
         with st.sidebar:
             selected = option_menu("Main Menu", ["All","Skills Lab Inventory", "Dean's Office Inventory", 'Stock Room Inventory', 'Models Inventory', 'Microscope Inventory'], 
                 icons=['journal-medical', 'journal-medical', 'journal-medical', 'journal-medical', 'journal-medical', 'journal-medical'], menu_icon="calendar4-week", default_index=0)
-        st.title("Inventory") 
-        rows = InventoryData.load_data()
-        df = pd.DataFrame(rows)
-        data = df[['Item','Quantity','Commonly_Used_By','Location']]
+        
+        if selected == "All" and index_title == None:
+            index_title = "All"
+
         #data2 = data[data['Location'] == "Stock Room"]
         #data.index+=1
-        st.write("Stock Room")
+        st.title(index_title)
         st.table(data)
 
         st.markdown(""" <style>
