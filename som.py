fb57e464e0a0c9b05c2cc83aa8c1c884d3933d9d
Link <clcreta@ctemplar.com>
2022-06-03 13:57:28 +0800

    refactoring code

--- som.py
+++ som.py
@@ -1,28 +1,9 @@
 import streamlit as st
 import pandas as pd
-from google.oauth2 import service_account
-from gsheetsdb import connect
 
-# Create a connection object.
-credentials = service_account.Credentials.from_service_account_info(
-    st.secrets["gcp_service_account"],
-    scopes=[
-        "https://www.googleapis.com/auth/spreadsheets",
-    ],
-)
-conn = connect(credentials=credentials)
+from inventorydata import InventoryData
 
-# Perform SQL query on the Google Sheet.
-# Uses st.cache to only rerun when the query changes or after 10 min.
-@st.cache(ttl=600)
-def run_query(query):
-    rows = conn.execute(query, headers=1)
-    rows = rows.fetchall()
-    return rows
-
-sheet_url = st.secrets["private_gsheets_url"]
-rows = run_query(f'SELECT * FROM "{sheet_url}"')
-
+rows = InventoryData.load_data()
 df = pd.DataFrame(rows)
 data = df[['Item','Quantity','Commonly_Used_By','Location']]
 data.index+=1
