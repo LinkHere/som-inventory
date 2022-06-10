216382d8ac251c8dd491b0754610acaeaf220604
LinkHere <99461450+LinkHere@users.noreply.github.com>
2022-06-10 15:04:11 +0800

    Update inventorydata.py

--- inventorydata.py
+++ inventorydata.py
@@ -5,13 +5,13 @@ from gsheetsdb import connect
 
 credentials = DataBConnection.load_credentials()
 conn = connect(credentials=credentials)
+sheet_url = st.secrets["private_gsheets_url"]
+query = f'SELECT * FROM "{sheet_url}"'
 
 class InventoryData:
 
-    @st.cache(hash_funcs={"_thread.RLock": lambda _: None})
+    @st.cache(ttl=600)
     def load_data():
-        sheet_url = st.secrets["private_gsheets_url"]
-        query = f'SELECT * FROM "{sheet_url}"'
         rows = conn.execute(query, headers=1)
         rows = rows.fetchall()
         return rows
