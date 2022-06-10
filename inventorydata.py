2a44240ce3a3ba3899c82a46242058faa42bc512
LinkHere <99461450+LinkHere@users.noreply.github.com>
2022-06-10 15:06:53 +0800

    Update inventorydata.py

--- inventorydata.py
+++ inventorydata.py
@@ -10,7 +10,7 @@ query = f'SELECT * FROM "{sheet_url}"'
 
 class InventoryData:
 
-    @st.cache(ttl=600)
+    #@st.cache(ttl=600)
     def load_data():
         rows = conn.execute(query, headers=1)
         rows = rows.fetchall()
