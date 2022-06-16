d1d83be54bda8da34c870abed2e0e455802d7d95
Link <clcreta@ctemplar.com>
2022-06-16 09:46:19 +0800

    Some Changes

--- src/som.py
+++ src/som.py
@@ -77,7 +77,7 @@ class SomInventory:
             data['Item'] = data['Item'].str.title()
             data.index = data.index.factorize()[0] + 1
             data = data[['Item', 'Quantity', 'Commonly_Used_By', 'Location']]
-            image = Image.open('https://raw.githubusercontent.com/LinkHere/inventory-images-models/blob/2c1b7e0cbf2c203af4772547fbefd35ff2f5dfe8/OrgChart.jpg')
+            image = Image.open('https://raw.githubusercontent.com/LinkHere/inventory-images-models/main/OrgChart.jpg')
             st.image(image, caption='Sunrise by the mountains')
             
         if selected == "Tools" and index_title == None:
