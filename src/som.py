e165b045cc8ac93af4524a5eb3c41d9e48de3407
LinkHere <99461450+LinkHere@users.noreply.github.com>
2022-06-18 15:33:56 +0800

    Update som.py

--- src/som.py
+++ src/som.py
@@ -108,7 +108,7 @@ class SomInventory:
 #         st.title(index_title)
 #         st.table(data)
         gb = GridOptionsBuilder.from_dataframe(data)
-        gb.configure_pagination(paginationAutoPageSize=True) #Add pagination
+        gb.configure_pagination(paginationAutoPageSize=20) #Add pagination
         gb.configure_side_bar() #Add a sidebar
         gb.configure_selection('multiple', use_checkbox=True, groupSelectsChildren="Group checkbox select children") #Enable multi-row selection
         gridOptions = gb.build()
