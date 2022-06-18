caf3cd8634374b17e5c1672a3ca50bc1089889dd
LinkHere <99461450+LinkHere@users.noreply.github.com>
2022-06-18 15:28:56 +0800

    Update som.py

--- src/som.py
+++ src/som.py
@@ -5,7 +5,7 @@ import requests
 from io import BytesIO
 from inventorydata import InventoryData
 from PIL import Image
-from st_aggrid import AgGrid
+from st_aggrid import GridOptionsBuilder, AgGrid, GridUpdateMode, DataReturnMode
 from streamlit_option_menu import option_menu
 
 class SomInventory:
@@ -107,7 +107,29 @@ class SomInventory:
 
         st.title(index_title)
         st.table(data)
-        AgGrid(data)
+        gb = GridOptionsBuilder.from_dataframe(data)
+        gb.configure_pagination(paginationAutoPageSize=True) #Add pagination
+        gb.configure_side_bar() #Add a sidebar
+        gb.configure_selection('multiple', use_checkbox=True, groupSelectsChildren="Group checkbox select children") #Enable multi-row selection
+        gridOptions = gb.build()
+
+        grid_response = AgGrid(
+            data,
+            gridOptions=gridOptions,
+            data_return_mode='AS_INPUT', 
+            update_mode='MODEL_CHANGED', 
+            fit_columns_on_grid_load=False,
+            theme='blue', #Add theme color to the table
+            enable_enterprise_modules=True,
+            height=350, 
+            width='100%',
+            reload_data=True
+            )
+
+        data = grid_response['data']
+        selected = grid_response['selected_rows'] 
+        df = pd.DataFrame(selected) #Pass the selected rows to a new dataframe df
+        AgGrid(df)
 
 st.set_page_config(
     page_title="SOM-Inventory",
