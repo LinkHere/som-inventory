ff181d1ed47b15aa0d0b9c45d64e25f1a63efef5
Link <clcreta@ctemplar.com>
2022-06-21 14:50:35 +0800

    Some Changes

--- src/inventorydata.py
+++ src/inventorydata.py
@@ -7,12 +7,12 @@ class InventoryData:
     
     global conn, query
     
-    credentials = DataBConnection.load_credentials()
-    sheet_url = st.secrets["private_gsheets_url"]
-    conn = connect(credentials=credentials)
-    query = f'SELECT * FROM "{sheet_url}"'
-    
+    @st.experimental_singleton(show_spinner=True)
     def load_data():
+        credentials = DataBConnection.load_credentials()
+        sheet_url = st.secrets["private_gsheets_url"]
+        conn = connect(credentials=credentials)
+        query = f'SELECT * FROM "{sheet_url}"'
         rows = conn.execute(query, headers=1)
         rows = rows.fetchall()
         return rows
