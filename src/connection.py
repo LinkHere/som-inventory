3ddfce2f9a01614301f946e698c15bc3023240f3
Link <clcreta@ctemplar.com>
2022-06-22 09:47:21 +0800

    Some Changes

--- src/connection.py
+++ src/connection.py
@@ -2,7 +2,7 @@ import streamlit as st
 from google.oauth2 import service_account
 
 class DataBConnection:
-    @st.cache(hash_funcs={_thread.RLock: my_hash_func})
+    @st.cache
     def load_credentials():
         credentials = service_account.Credentials.from_service_account_info(
             st.secrets["gcp_service_account"],
